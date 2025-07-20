6f1a613ac7c010820e857535f73072c638bf54d0
Sivanim20 <sivanimanju2003@gmail.com>
2025-07-20 23:20:58 +0530

    updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Navbar from './navbar'
 import { motion } from 'framer-motion'
+import headerImage from '../assets/header.png';
 
 const Header = () => {
   return (
-    <div className='min-h-screen mb-4 bg-cover bg-center flex 
-    items-center w-full overflow-hidden' style={{backgroundImage:"url('/header_img.png')"}} id='Header'>
+    <div className='min-h-screen mb-4 bg-cover bg-gray-600 bg-center flex 
+    items-center w-full overflow-hidden' style={{backgroundImage: `url(${headerImage})`}} id='Header'>
         <Navbar/>
-        <motion.div initial={{opacity:0, y:100}} 
+        <motion.div 
+          initial={{opacity:0, y:100}} 
           transition={{duration:1.5}}
           whileInView={{opacity:1, y:0}}
           viewport={{once:true}}
